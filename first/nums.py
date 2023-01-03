1694ffa5dcb35d9f985f41643d9e334e78931a63
Nikita <nikvs@mail.ru>
2023-01-03 16:19:14 +0700

    add def mul, add module strings.py

--- first/nums.py
+++ first/nums.py
@@ -6,8 +6,12 @@ def minus(a, b):
     return a - b
 
 
-text = "new text"
+def mul(a, b):
+    return a * b
+
 
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
+    print(mul(3, 3))
+
